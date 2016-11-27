184ceed8d2bb644cc87b4712837bcff4a79c1d43
Jon Balon <development@jonbalon.com>
2016-11-26 20:43:19 -0500

    Worked on /login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -8,9 +8,12 @@ mongoose.createConnection(dbConfig['connString']);
 // Database model.
 const User = require('../models/User');
 
+// Module for generating hashed passwords.
+const hashedAuthentication = require('../authentication/userLogin');
+
 // Route definition.
 const login = (req, res, next) => {
-  
+
   // Get username and password.
   const username = req.body.username;
   const password = req.body.password;
@@ -20,7 +23,45 @@ const login = (req, res, next) => {
     return next('Both a username and password are required.');
   }
 
-  res.json('login route');
+  // Access the database.
+  User.findOne({username: username}, (error, result) => {
+
+    // Error check.
+    if (error) { return next('Login failed. Unable to access database.'); }
+
+    // Check if a matching user was found.
+    if (result) {
+
+      // Get the user's salt from the database.
+      const userSalt = result.password.salt;
+
+      // Get the user's hashed password from the database.
+      const userHashedPassword = result.password.hash;
+
+      // Generate a hashed password using given plaintext password
+      // and salt.
+      const generatedHashedPassword = hashedAuthentication.generateHashedPassword(password, userSalt).hash;
+
+      // Compare the stored hashed password with the generated one.
+      if (userHashedPassword == generatedHashedPassword) {
+
+        // Passwords match, login successful.
+        return res.json('login successful');
+
+      } else {
+
+        // Passwords do not match, return an error.
+        return next('Invalid username or password.');
+      }
+
+    } else {
+
+      // No user found, just return an error stating invalid username or password.
+      return next('Invalid username or password');
+    }
+
+  });
+
 }
 
 // Export route definition.
