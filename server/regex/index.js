ec3d3b1a2bbbbc86d55880d30368f27b395e4655
Jon Balon <development@jonbalon.com>
2016-11-30 16:51:11 -0500

    Poll titles and choices can use any character

--- server/regex/index.js
+++ server/regex/index.js
@@ -2,9 +2,8 @@ const rules = {
   username: /^[\w\d]{8,25}$/,
   password: /^[\w\d]{8,50}$/,
   POLL: {
-    TITLE_STRING: /^[\w\d\s]{8,1000}$/,
-    CHOICE_STRING: /^[\w\d\s]{1,30}$/,
-    OPTION_REQ_HEADER: /^option[\d]+$/
+    TITLE_STRING: /^.{8,1000}$/,
+    CHOICE_STRING: /^.{1,50}$/
   }
 }
 
