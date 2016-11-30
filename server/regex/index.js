9f62a4a0da25c2f5cb06558209105220bb2e2da2
Jon Balon <development@jonbalon.com>
2016-11-30 05:32:25 -0500

    allow spaces in title and choice strings

--- server/regex/index.js
+++ server/regex/index.js
@@ -2,8 +2,8 @@ const rules = {
   username: /^[\w]{8,25}$/,
   password: /^[\w]{8,50}$/,
   POLL: {
-    TITLE_STRING: /^[\w]{8,50}$/,
-    CHOICE_STRING: /^[\w]{1,30}$/,
+    TITLE_STRING: /^[\w\s]{8,50}$/,
+    CHOICE_STRING: /^[\w\s]{1,30}$/,
     OPTION_REQ_HEADER: /^option[\d]+$/
   }
 }
