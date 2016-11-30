1af4299f4bc2f7a83785396f3be67a3933a52fab
Jon Balon <development@jonbalon.com>
2016-11-30 11:36:14 -0500

    Added verbose messages to responses

--- server/responseMessages/index.js
+++ server/responseMessages/index.js
@@ -7,35 +7,35 @@ const response = (code, message) => {
 
 const errorResponses = {
     LOGIN: {
-      MISSING_CREDENTIALS: response('MISSING_CREDENTIALS'),
-      INVALID_CREDENTIALS: response('INVALID_CREDENTIALS')
+      MISSING_CREDENTIALS: response('MISSING_CREDENTIALS', 'Both username and password are required.'),
+      INVALID_CREDENTIALS: response('INVALID_CREDENTIALS', 'Incorrect username or password')
     },
     SIGNUP: {
-      MISSING_CREDENTIALS: response('MISSING_CREDENTIALS'),
-      INVALID_USERNAME: response('INVALID_USERNAME'),
-      INVALID_PASSWORD:  response('INVALID_PASSWORD'),
-      EXISTING_USER:  response('EXISTING_USER')
+      MISSING_CREDENTIALS: response('MISSING_CREDENTIALS', 'Bother username and password are required.'),
+      INVALID_USERNAME: response('INVALID_USERNAME', 'Username can only contain letters and numbers (dashes and underscores are allowed), between 8 and 25 characters long.'),
+      INVALID_PASSWORD:  response('INVALID_PASSWORD', 'Password can only contain letters and numbers (dashes and underscores are allowed), between 8 and 50 characters long.'),
+      EXISTING_USER:  response('EXISTING_USER', 'A user with that username already exists.')
     },
     TOKEN: {
-      MISSING_TOKEN: response('MISSING_TOKEN'),
-      INVALID_TOKEN: response('INVALID_TOKEN'),
-      EXPIRED_TOKEN: response('EXPIRED_TOKEN'),
+      MISSING_TOKEN: response('MISSING_TOKEN', 'A login token is required.'),
+      INVALID_TOKEN: response('INVALID_TOKEN', 'Login token is invalid.'),
+      EXPIRED_TOKEN: response('EXPIRED_TOKEN', 'Login token has expired.'),
     },
     POLL: {
-      INVALID_TITLE: response('INVALID_TITLE'),
-      NO_TITLE: response('NO_TITLE'),
+      INVALID_TITLE: response('INVALID_TITLE', 'Poll name can only contain letters, numbers, spaces, and must be between 8 and 1000 characters long.'),
+      NO_TITLE: response('NO_TITLE', 'Poll name is required.'),
     },
     DB: {
-      DB_ERROR: response('DB_ERROR'),
+      DB_ERROR: response('DB_ERROR', 'Server error. Unable to connect to the database'),
     }
 }
 
 const successResponses = {
   SIGNUP: {
-    USER_CREATED: response('USER_CREATED')
+    USER_CREATED: response('USER_CREATED', 'User successfully created.')
   },
   POLL: {
-    POLL_CREATED: response('POLL_CREATED')
+    POLL_CREATED: response('POLL_CREATED', 'Poll successfully created.')
   }
 }
 
