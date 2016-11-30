81c859a74a985984eec0333c269072e79fc5c7b6
Jon Balon <development@jonbalon.com>
2016-11-30 01:38:43 -0500

    Added choice string length

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -2,7 +2,8 @@ const appConfig = {
   'JWT_SIGNING_KEY': process.env['APP_JWT_SIGNING_KEY'],
   'JWT_TOKEN_EXPIRATION_SECONDS': 86400,
   'POLL': {
-    'MAX_CHOICES': 10
+    'MAX_CHOICES': 10,
+    'CHOICE_STRING_LENGTH': 30,
   }
 }
 
