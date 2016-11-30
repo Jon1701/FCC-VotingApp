22b940128ed953600b0130e092c9cf24c6fc005d
Jon Balon <development@jonbalon.com>
2016-11-29 21:26:08 -0500

    Middleware for verifying json web tokens

--- server/middleware/verifyJsonWebToken.js
+++ server/middleware/verifyJsonWebToken.js
@@ -1,5 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+// Modules
+////////////////////////////////////////////////////////////////////////////////
+const jwt = require('jsonwebtoken');  // JSON web token.
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////////////////////////////////////////
+const ERROR_RESPONSE = require('../responseMessages/error');  // Error Responses.
+const APP_CONFIG = require('../config/appConfig');  // Application configuration variables
+
+////////////////////////////////////////////////////////////////////////////////
+// Middleware definition
+////////////////////////////////////////////////////////////////////////////////
 const verifyJsonWebToken = (req, res, next) => {
-  return res.json('middleware to verify json web token');
+
+  // Access the token in the request header.
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // Check for the existance of a json web token within the request header.
+  if (token) {
+
+    // If a token is provided, check its validity by decoding it using the
+    // key used to sign it.
+    jwt.verify(token, APP_CONFIG.JWT_SIGNING_KEY, (err, decoded) => {
+
+      // Check if the token is valid.
+      if (err) {
+
+        // Token is invalid, return error as response.
+        return res.json(ERROR_RESPONSE.TOKEN.INVALID_TOKEN);
+
+      } else {
+
+        // Token is valid, store it for future use.
+        req.decoded = decoded;
+
+        // Go to next middleware.
+        next();
+
+      }
+
+    });
+
+  } else {
+
+    // No token provided, return error.
+    return res.json(ERROR_RESPONSE.TOKEN.NO_TOKEN);
+
+  }
+
 }
 
+// Export middleware.
 module.exports = verifyJsonWebToken;
