ebb0691932838a8949df38ffc175b693294e5b3d
Jon Balon <development@jonbalon.com>
2016-11-30 02:26:58 -0500

    Removed string lengths

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -1,10 +1,6 @@
 const appConfig = {
   'JWT_SIGNING_KEY': process.env['APP_JWT_SIGNING_KEY'],
   'JWT_TOKEN_EXPIRATION_SECONDS': 86400,
-  'POLL': {
-    'MAX_CHOICES': 10,
-    'CHOICE_STRING_LENGTH': 30,
-  }
 }
 
 module.exports = appConfig;
