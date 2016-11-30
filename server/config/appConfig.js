8e5e99d3f3c930151969379ef783edc9036a2d70
Jon Balon <development@jonbalon.com>
2016-11-30 01:25:26 -0500

    Added maximum number of choices per poll

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -1,6 +1,9 @@
 const appConfig = {
   'JWT_SIGNING_KEY': process.env['APP_JWT_SIGNING_KEY'],
-  'JWT_TOKEN_EXPIRATION_SECONDS': 86400
+  'JWT_TOKEN_EXPIRATION_SECONDS': 86400,
+  'POLL': {
+    'MAX_CHOICES': 10
+  }
 }
 
 module.exports = appConfig;
