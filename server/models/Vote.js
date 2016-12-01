0bf0b00970fc3c9dedb26aef4b10809e9b9361b0
Jon Balon <development@jonbalon.com>
2016-11-30 21:49:01 -0500

    Added db model for a vote

---
+++ server/models/Vote.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Vote = mongoose.model('Vote', new Schema({
+  username: String,
+  poll_id: String,
+  choice: String
+}));
+
+module.exports = Vote;
