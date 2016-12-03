fa2988dcef9a7a6f1a8ef3b5f70d28b47b12fc7c
Jon Balon <development@jonbalon.com>
2016-12-02 20:49:20 -0500

    Use aggregation framework to tally up results

--- server/routes/view/poll.js
+++ server/routes/view/poll.js
@@ -16,7 +16,12 @@ const REGEX = require('../../regex/index');                         // Regular e
 mongoose.createConnection(DB_CONFIG['CONN_STRING']);  // Connect to the database.
 
 // Models.
-const Poll = require('../../models/Poll.js');
+const Poll = require('../../models/Poll.js'); // Poll
+const Vote = require('../../models/Vote.js'); // Vote
+
+const generateAggregatedResults = () => {
+
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Route definition
@@ -39,7 +44,63 @@ const poll = (req, res, next) => {
       // If no poll was found, return an error.
       if (!resultPoll) { return next(RESPONSE.ERROR.VIEW_POLL.NO_POLL_FOUND); }
 
-      // A poll has been found, tally up the votes.
+      // Tally up the votes in the poll.
+      Vote.aggregate([
+        {
+          $match: {         //
+            poll_id: pollId // Search for matching poll id.
+          }                 //
+        },
+        {
+          $group: {
+            _id: '$choice', // Group by choice.
+            count: {  //
+              $sum: 1 // Count number of occurrences.
+            }         //
+          }
+        }
+      ], (err, result) => {
+
+        // Error check.
+        if (err) { return next(RESPONSE.ERROR.DB.DB_ERROR); }
+
+        // Check if results were tallied.
+        if (result.length > 0) {
+
+          // Votes were made to this poll, tally up results.
+          // Restructure the resulting document.
+          const newResult = {};
+          for (let i=0; i<result.length; i++) {
+            let choice = result[i]['_id'];  // Poll choice
+            let counts = result[i]['count'];// Choice count
+
+            // Store in newResult.
+            newResult[choice] = counts;
+          }
+
+          // Return results to user.
+          return res.send(Object.assign({}, RESPONSE.SUCCESS.VIEW_POLL.VIEW_RESULTS, {results: newResult}));
+
+        } else {
+
+          // Restructure the resulting document.
+          const newResult = {};
+          for (let i=0; i<resultPoll.choices.length; i++) {
+
+            // Poll choice.
+            const choice = resultPoll.choices[i];
+
+            // Store poll choice with count 0.
+            newResult[choice] = 0;
+          }
+
+          // Return results to user.
+          return res.send(Object.assign({}, RESPONSE.SUCCESS.VIEW_POLL.VIEW_RESULTS, {results: newResult}));
+
+        }
+
+
+      });
 
     });
 
