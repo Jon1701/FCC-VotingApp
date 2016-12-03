261f4e8655f296aea7960cdc6512ef69c813e4cb
Jon Balon <development@jonbalon.com>
2016-12-02 19:10:51 -0500

    Added check for poll existence

--- server/routes/view/poll.js
+++ server/routes/view/poll.js
@@ -15,14 +15,34 @@ const REGEX = require('../../regex/index');                         // Regular e
 ////////////////////////////////////////////////////////////////////////////////
 mongoose.createConnection(DB_CONFIG['CONN_STRING']);  // Connect to the database.
 
+// Models.
+const Poll = require('../../models/Poll.js');
+
 ////////////////////////////////////////////////////////////////////////////////
 // Route definition
 ////////////////////////////////////////////////////////////////////////////////
 const poll = (req, res, next) => {
 
+  // Get the poll ID from the request parameters.
+  const pollId = req.params.poll_id;
+
   // Check to see if the poll_id URL parameter is a valid ObjectId.
-  if (mongoose.Types.ObjectId.isValid(req.params.poll_id)) {
-    return res.send('valid objectid')
+  if (mongoose.Types.ObjectId.isValid(pollId)) {
+
+    // Valid ObjectId for a Pol' was returned, search the polls collection for a
+    // poll with that ObjectId.
+    Poll.findOne({_id: pollId}, (err, resultPoll) => {
+
+      // Error check.
+      if (err) { return next(RESPONSE.ERROR.DB.DB_ERROR); }
+
+      // If no poll was found, return an error.
+      if (!resultPoll) { return next(RESPONSE.ERROR.VIEW_POLL.NO_POLL_FOUND); }
+
+      // A poll has been found, tally up the votes.
+
+    });
+
   } else {
 
     // Invalid poll ID, return an error.
@@ -30,9 +50,6 @@ const poll = (req, res, next) => {
 
   }
 
-  console.log(mongoose.Types.ObjectId.isValid)
-
-  return res.json('Reached endpoint /view/poll/:poll_id')
 }
 
 // Export route definition.
