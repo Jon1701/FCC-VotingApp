134cd221ebc396b71abd6a006a4a545f87352f25
Jon Balon <development@jonbalon.com>
2016-12-05 21:15:54 -0500

    Added store and reducers

--- ui/index.js
+++ ui/index.js
@@ -2,10 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux.
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// Reducers and store.
+import reducers from 'reducers/index.js';
+let store = createStore(reducers);
+
 // React container.
 import UserInterface from 'containers/UserInterface.jsx';
 
 // Add stylesheets.
 require("stylesheets/stylesheet.scss");
 
-ReactDOM.render(<UserInterface/>, document.getElementById('react-target'));
+ReactDOM.render(<Provider store={store}><UserInterface/></Provider>, document.getElementById('react-target'));
