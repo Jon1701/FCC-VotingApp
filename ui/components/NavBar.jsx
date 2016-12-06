c2cc5b5f3092a24f325f10c119644718e0b13268
Jon Balon <development@jonbalon.com>
2016-12-05 19:08:47 -0500

    Temporary navbar

---
+++ ui/components/NavBar.jsx
@@ -0,0 +1,20 @@
+// React.
+import React from 'react';
+
+// Component definition.
+export default class NavBar extends React.Component {
+
+  // Component constructor.
+  constructor(props) {
+    super(props);
+  }
+
+  // Component render.
+  render() {
+    return (
+      <div>
+        NavBar
+      </div>
+    )
+  }
+}
