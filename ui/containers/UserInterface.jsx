7e3832230c97ab20a617e52b3f5f17f97907b824
Jon Balon <development@jonbalon.com>
2016-12-11 19:47:52 -0500

    Now uses React Router

--- ui/containers/UserInterface.jsx
+++ ui/containers/UserInterface.jsx
@@ -9,6 +9,10 @@ import LoginPage from 'containers/LoginPage';
 import SignupPage from 'containers/SignupPage';
 import CreatePoll from 'containers/CreatePoll';
 import ViewPoll from 'containers/ViewPoll';
+import App from 'containers/App';
+
+// React Router
+import { Router, Route, Link, browserHistory, IndexRoute, hashHistory } from 'react-router';
 
 // Component definition.
 export default class UserInterface extends React.Component {
@@ -21,13 +25,24 @@ export default class UserInterface extends React.Component {
   // Component render.
   render() {
     return (
-      <div>
-        <NavBar/>
-        <ViewPoll/>
-        <LoginPage/>
-        <CreatePoll/>
-        <SignupPage/>
-      </div>
+      <Router history={browserHistory}>
+        <Route path='/' component={App}>
+          <Route path='/login' component={LoginPage}/>
+          <Route path='/signup' component={SignupPage}/>
+          <Route path='/create_poll' component={CreatePoll}/>
+          <Route path='/view/poll/:poll_id' component={ViewPoll}/>
+        </Route>
+      </Router>
     )
   }
 }
+
+/*
+<div>
+  <NavBar/>
+  <ViewPoll/>
+  <LoginPage/>
+  <CreatePoll/>
+  <SignupPage/>
+</div>
+*/
