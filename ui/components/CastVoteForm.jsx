c4d97d71a7b3b2cee89d935ac27253f64a34d423
Jon Balon <development@jonbalon.com>
2016-12-14 02:38:36 -0500

    Added functionality to cast votes

---
+++ ui/components/CastVoteForm.jsx
@@ -0,0 +1,152 @@
+////////////////////////////////////////////////////////////////////////////////
+// React
+////////////////////////////////////////////////////////////////////////////////
+import React from 'react';
+
+// Components
+import AlertBox from 'components/AlertBox';
+
+////////////////////////////////////////////////////////////////////////////////
+// Redux
+////////////////////////////////////////////////////////////////////////////////
+import { connect } from 'react-redux';
+
+////////////////////////////////////////////////////////////////////////////////
+// Other modules
+////////////////////////////////////////////////////////////////////////////////
+
+// External libraries.
+import classNames from 'classnames';
+import axios from 'axios';  // AJAX Request library.
+const CONFIG_AXIOS = require('config/axios.json');  // Axios configuration file.
+
+////////////////////////////////////////////////////////////////////////////////
+// Component definition
+////////////////////////////////////////////////////////////////////////////////
+class CastVoteForm extends React.Component {
+
+  // Component constructor.
+  constructor(props) {
+    super(props)
+
+    // Component state.
+    this.state = {
+      selectedChoice: null, // Currently selected choice.
+    }
+
+    // Bind methods to component instance.
+    this.handleSelectedChoice = this.handleSelectedChoice.bind(this);
+    this.updateAlertBox = this.updateAlertBox.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  // Method to store the currently selected poll choice in state.
+  handleSelectedChoice(e) {
+    this.setState({
+      selectedChoice: e.target.getAttribute('value')
+    })
+  }
+
+  // Function to update Alert Box.
+  updateAlertBox(alertBoxMessage, alertBoxType) {
+    this.setState({
+      alertBoxMessage: alertBoxMessage,
+      alertBoxType: alertBoxType
+    })
+  }
+
+  // Function to handle form submit (cast vote).
+  handleFormSubmit(e) {
+
+    // Prevent default form submit action.
+    e.preventDefault();
+
+    // Hide standard alertbox.
+    this.updateAlertBox(null, null);
+
+    // Create axios instance.
+    // Send JSON Web Token in the header.
+    const request = axios.create({
+      headers: {
+        'x-access-token': this.props.token
+      }
+    });
+
+    // Send Poll ID and choice with the request.
+    const data = {
+      choice: this.state.selectedChoice,
+      poll_id: this.props.poll_id
+    }
+
+    // Send a GET request to the server to get poll information.
+    request.post('/api/auth/cast_vote', data, CONFIG_AXIOS)
+    .then((res) => {
+
+      // Hide standard alertbox.
+      this.updateAlertBox('Vote successfully cast.', 'SUCCESS');
+
+    }).catch((err) => {
+
+      // Display error message.
+      this.updateAlertBox(err.response.data.message, 'DANGER');
+
+    });
+
+  }
+
+  // Component render.
+  render() {
+
+    // Build HTML containing poll choices.
+    const choices = this.props.pollData.choices.map((choice, idx, arr) => {
+
+      // Classes to control style and visibility of currently clicked choice.
+      let myClasses = classNames({
+        'button width-50': true,
+        'is-primary': choice == this.state.selectedChoice
+      })
+
+      // Return a styled <div> for each choice.
+      return (
+        <div key={'choice_'+idx} className="width-100">
+          <a className={myClasses} value={choice} onClick={this.handleSelectedChoice}>{choice}</a>
+        </div>
+      )
+    });
+
+    // Component render.
+    return (
+      <div>
+
+        <AlertBox message={this.state.alertBoxMessage} boxType={this.state.alertBoxType}/>
+
+        <h1 className='title'>{this.props.pollData.title}</h1>
+
+        <form>
+          <div>
+            {choices}
+          </div>
+
+          <br/>
+
+          <div className="width-100 text-center">
+            <a className="width-50 button is-primary" ref="submitButton" onClick={this.handleFormSubmit}>Cast Vote</a>
+          </div>
+        </form>
+
+      </div>
+    )
+  }
+
+}
+
+
+// Maps state to props.
+const mapStateToProps = (state) => {
+  return {
+    token: state.token
+  }
+}
+
+// Allow component access to Redux store.
+export default connect(mapStateToProps, null)(CastVoteForm);
--- ui/containers/UserInterface.jsx
+++ ui/containers/UserInterface.jsx
@@ -9,6 +9,7 @@ import LoginPage from 'containers/LoginPage';
 import SignupPage from 'containers/SignupPage';
 import CreatePoll from 'containers/CreatePoll';
 import ViewPoll from 'containers/ViewPoll';
+import CastVote from 'containers/CastVote';
 import App from 'containers/App';
 
 // React Router
@@ -31,6 +32,7 @@ export default class UserInterface extends React.Component {
           <Route path='/signup' component={SignupPage}/>
           <Route path='/create_poll' component={CreatePoll}/>
           <Route path='/view/poll/:poll_id' component={ViewPoll}/>
+          <Route path='/vote/poll/:poll_id' component={CastVote}/>
         </Route>
       </Router>
     )
