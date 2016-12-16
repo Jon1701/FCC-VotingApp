4fd5c50bd14b57f7ffd52fc222571222844ef793
Jon Balon <development@jonbalon.com>
2016-12-15 22:31:29 -0500

    Added creation date

--- server/models/Vote.js
+++ server/models/Vote.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const Vote = mongoose.model('Vote', new Schema({
   username: String,
   poll_id: String,
-  choice: String
+  choice: String,
+  creationDate: { type: Date, default: Date.now }
 }));
 
 module.exports = Vote;
