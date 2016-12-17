0a3004b68da90efaf2291d59c3cdcb8ae9d2a8ca
Jon Balon <development@jonbalon.com>
2016-12-16 21:28:54 -0500

    Started rewrite on v2 UI, all old components/containers moved to legacy folders, will be deleted near end of v2 UI, <UserInterface/> stripped to basic framework

--- ui/containers/UserInterface.jsx
+++ ui/containers/UserInterface.jsx
@@ -1,23 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
 // React
+////////////////////////////////////////////////////////////////////////////////
 import React from 'react';
 
-// Components.
-import NavBar from 'components/NavBar';
-
 // Containers.
-import LoginPage from 'containers/LoginPage';
-import SignupPage from 'containers/SignupPage';
-import CreatePoll from 'containers/CreatePoll';
-import ViewPoll from 'containers/ViewPoll';
-import CastVote from 'containers/CastVote';
 import App from 'containers/App';
-import AppIndex from 'containers/AppIndex';
-import Dashboard from 'containers/Dashboard';
 
+////////////////////////////////////////////////////////////////////////////////
 // React Router
+////////////////////////////////////////////////////////////////////////////////
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-// Component definition.
+////////////////////////////////////////////////////////////////////////////////
+// Component Definition
+////////////////////////////////////////////////////////////////////////////////
+
 export default class UserInterface extends React.Component {
 
   // Component constructor.
@@ -30,13 +27,7 @@ export default class UserInterface extends React.Component {
     return (
       <Router history={hashHistory}>
         <Route path='/' component={App}>
-          <IndexRoute component={AppIndex}/>
-          <Route path='/login' component={LoginPage}/>
-          <Route path='/signup' component={SignupPage}/>
-          <Route path='/view/poll/:poll_id' component={ViewPoll}/>
-          <Route path='/auth' component={Dashboard}/>
-          <Route path='/auth/create_poll' component={CreatePoll}/>
-          <Route path='/auth/vote/poll/:poll_id' component={CastVote}/>
+
         </Route>
       </Router>
     )
