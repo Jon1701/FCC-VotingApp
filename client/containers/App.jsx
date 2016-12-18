aefc0e9ab392aac02df7b3493c5cdf4e252172aa
Jon Balon <development@jonbalon.com>
2016-12-18 07:21:08 -0500

    made component redux-aware, now reads token from session storage

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -7,13 +7,32 @@ import React from 'react';
 import NavigationBar from 'components/NavigationBar'; // Navigation Bar.
 
 ////////////////////////////////////////////////////////////////////////////////
+// Redux
+////////////////////////////////////////////////////////////////////////////////
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Actions.
+import { storeToken } from 'actions/index';
+
+////////////////////////////////////////////////////////////////////////////////
 // Component definition
 ////////////////////////////////////////////////////////////////////////////////
-export default class App extends React.Component {
+class App extends React.Component {
 
   // Constructor.
   constructor(props) {
     super(props);
+  }
+
+  // Component Lifecycle Method.
+  //
+  // Execute this block when component is about to perform initial render.
+  componentWillMount() {
+
+    // Read token from session storage, store in application state.
+    this.props.storeToken(sessionStorage.getItem('token'));
+    
   }
 
   // Render.
@@ -31,3 +50,13 @@ export default class App extends React.Component {
   }
 
 }
+
+// Allow access of dispatch actions as props.
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    storeToken: storeToken
+  }, dispatch);
+}
+
+// Allow component access to Redux store.
+export default connect(null, mapDispatchToProps)(App);
