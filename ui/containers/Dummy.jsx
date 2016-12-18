fb642991248360e584d5b49f8f16aed9f3f4914d
Jon Balon <development@jonbalon.com>
2016-12-17 22:33:57 -0500

    Converted to functional component, moved to containers/ folder

---
+++ ui/containers/Dummy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Dummy extends React.Component {
+  render() {
+    return (
+      <div>
+        This is a Dummy Component.
+      </div>
+    )
+  }
+}
