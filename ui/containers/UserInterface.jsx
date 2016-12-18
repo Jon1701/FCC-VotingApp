0b44d03faaceea06f56fe0e1def04662a6f838ea
Jon Balon <development@jonbalon.com>
2016-12-17 19:58:49 -0500

    Added few routes, dummy component

--- ui/containers/UserInterface.jsx
+++ ui/containers/UserInterface.jsx
@@ -3,8 +3,9 @@
 ////////////////////////////////////////////////////////////////////////////////
 import React from 'react';
 
-// Containers.
+// Containers
 import App from 'containers/App';
+import LoginPage from 'containers/LoginPage';
 
 ////////////////////////////////////////////////////////////////////////////////
 // React Router
@@ -14,7 +15,6 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 ////////////////////////////////////////////////////////////////////////////////
 // Component Definition
 ////////////////////////////////////////////////////////////////////////////////
-
 export default class UserInterface extends React.Component {
 
   // Component constructor.
@@ -27,9 +27,19 @@ export default class UserInterface extends React.Component {
     return (
       <Router history={hashHistory}>
         <Route path='/' component={App}>
-
+          <Route path='/login' component={LoginPage}/>
+          <Route path='/create_poll' component={Dummy}/>
+          <Route path='/signup' component={Dummy}/>
+          <Route path='/auth/dashboard' component={Dummy}/>
         </Route>
       </Router>
     )
   }
 }
+
+// Dummy component.
+const Dummy = () => (
+  <div>
+    Dummy
+  </div>
+)
